df1c4966bfcf87b665bef79b0e64d38ae3914e53
Emily Crook <Emily.A.Crook96@gmail.com>
2021-05-11 18:02:02 -0500

    table from SQL ran from npm

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// requires SQL
+const mysql = require('mysql2');
 // requires express
 const express = require('express');
 
@@ -9,12 +11,30 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'C0ldD@ze',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 // GET test to ensure the connection is working
 // app.get('/', (req, res) => {
 //     res.json({
 //         message: 'Hello World'
 //     });
 // });
+
+// Return all the data in the candidates table
+db.query('SELECT * FROM candidates', (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {

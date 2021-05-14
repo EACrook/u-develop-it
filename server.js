34061949dd3a963b1d823ebc25cd9325dc9e49a1
Emily Crook <Emily.A.Crook96@gmail.com>
2021-05-14 13:35:15 -0500

    add CRUD routes

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-// requires SQL
-const mysql = require('mysql2');
-// requires express
 const express = require('express');
+const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 
-// PORT designation and app expression
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -13,63 +11,106 @@ app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        // Your MySQL username,
-        user: 'root',
-        // Your MySQL password
-        password: 'C0ldD@ze',
-        database: 'election'
-    },
-    console.log('Connected to the election database.')
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'C0ldD@ze',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
 );
 
-// GET test to ensure the connection is working
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: 'Hello World'
-//     });
-// });
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT * FROM candidates`;
 
-// Return all the data in the candidates table
-db.query('SELECT * FROM candidates', (err, rows) => {
-    console.log(rows);
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
 });
 
-// GET a single candidate
-db.query('SELECT * FROM candidates WHERE id = 9', (err, row) => {
+// Get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
     if (err) {
-        console.log(err);
+      res.status(400).json({ error: err.message });
+      return;
     }
-    console.log(row);
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
 });
 
 // Delete a candidate
-db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
     if (err) {
-        console.log(err)
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
     }
-    console.log(result);
+  });
 });
 
 // Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-    VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
+app.post('/api/candidate', ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'industry_connected'
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
 
-db.query(sql, params, (err, result) => {
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+
+  db.query(sql, params, (err, result) => {
     if (err) {
-        console.log(err);
+      res.status(400).json({ error: err.message });
+      return;
     }
-    console.log(result);
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
 });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
-})
+  res.status(404).end();
+});
 
-// function that starts the Express.js server on port 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
